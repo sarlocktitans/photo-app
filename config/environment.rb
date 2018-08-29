c94347e4dfd38b3c5e28ae0335d661633568a4ef
rails udemycourse <sarlocktitans@gmail.com>
2018-08-29 15:51:39 +0530

    add devise and bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#ActionMailer::Base.smtp_settings = {
+#	:address => 'smtp.sendgrid.net',
+#	:port => '587'
+#	:authentication => :plain,
+#	:user_name => 
+#}
