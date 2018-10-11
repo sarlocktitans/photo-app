c22b72c3b91513cf7b378da03bb311df016a4cbf
rails udemycourse <sarlocktitans@gmail.com>
2018-10-11 18:09:17 +0530

    added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,18 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.smtp_settings = {
+    user_name:      'shippi13789@gmail.com',
+    password:       'shippi#123',
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+ 
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
